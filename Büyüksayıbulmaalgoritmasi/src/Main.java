3b40b5369110b72fabd7dc39e0e4925560d72443
hafsaduman <150665763+hafsaduman@users.noreply.github.com>
2023-11-13 14:56:02 +0300

    sayıhesaplma

---
+++ Büyüksayıbulmaalgoritmasi/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=25;
+		int c=30;
+		int EB=a;
+		
+		if(EB<b)
+		{
+			EB=b;
+		}
+		if(c>EB)
+		{
+			EB=c;
+		}
+		System.out.println("en büyük sayi= " + EB);
+
+}
+
+}
